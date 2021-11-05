cc5525f848659d0e0cfce652f4e2ef5ba4d45a57
A-A-Tyurin <avt2@yandex.ru>
2021-11-06 02:29:08 +0300

    Update entrypoint_web.sh

--- entrypoint_web.sh
+++ entrypoint_web.sh
@@ -4,8 +4,8 @@ while ! nc -z $DB_HOST $DB_PORT; do
     sleep 0.1
 done
 
-python3 manage.py migrate
-python3 manage.py init_superuser
-python3 manage.py collectstatic --no-input
+sudo python3 manage.py migrate
+sudo python3 manage.py init_superuser
+sudo python3 manage.py collectstatic --no-input
 
 exec "$@"
